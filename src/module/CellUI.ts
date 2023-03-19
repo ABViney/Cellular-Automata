451a3dc89f90f83dbf700d92737f1db6832271e8
ABViney <vineyadrian@gmail.com>
2023-03-19 16:19:36 -0400

    First version UI

--- src/module/CellUI.ts
+++ src/module/CellUI.ts
@@ -1,22 +1,24 @@
-import { Container, Rectangle, Renderer, Sprite, utils } from "pixi.js";
+import { Container, IRenderer, Rectangle, Renderer, Sprite, utils } from "pixi.js";
 import { Layout, LayoutStyles } from "@pixi/layout";
 import { CheckBox, FancyButton, Input, Slider } from "@pixi/ui";
 import { AutomataOptions } from "../utils/CellContracts";
 import { Cell } from "../utils/CellData";
 import AssetManager from "./CellAssets";
-import { NeighborhoodController } from "./UIComponents";
+import { NeighborhoodController, RuleController, UpdateController } from "./UIComponents";
 import UIComponentStyles from "./UIComponentStyles";
+import { CellGrid } from "./CellGrid";
 
 const hex2string = utils.hex2string;
 const string2hex = utils.string2hex;
 
 export class CellUI extends Container {
   // private config: Pick<AutomataOptions, "pattern"|"persists_at"|"populates_at">;
+  private asset_man: AssetManager;
 
   private layout: Layout;
 
   constructor(
-    asset_manager: AssetManager,
+    renderer: IRenderer
     // initial_rules: AutomataOptions,
     // renderer: Renderer,
     // onSearchPatternChange: (pattern: Cell[]) => void,
@@ -27,10 +29,52 @@ export class CellUI extends Container {
     // requestReset: () => void,
     // rebuildRules: () => AutomataOptions
   ) {
-
     super();
-    let neighborhood_controller = new NeighborhoodController(asset_manager);
-    this.layout= new Layout({
+
+    const asset_man = this.asset_man = new AssetManager(renderer);
+    // Child components
+    // const grid = new CellGrid(renderer, () => {});
+    const neighborhood_controller = new NeighborhoodController(asset_man);
+    const persist_input = new RuleController(asset_man);
+    const populate_input = new RuleController(asset_man);
+    const update_controller = new UpdateController(asset_man);
+
+    this.layout = new Layout(
+      this.formattedLayoutOptions(
+        // grid,
+        neighborhood_controller,
+        persist_input,
+        populate_input,
+        update_controller
+      )
+    );
+    this.addChild(this.layout);
+    this.on('added',() => {
+      this.resize(this.parent.width, this.parent.height)
+    });
+
+    // Until the Layout class has a bubbling refresh and/or content Containers emit a resize event--
+    // a content Container cannot be dynamically sized without invoking the resize method of its parent.
+    const pattern_controller_layout = this.layout.content.getByID('pattern_controller') as Layout;
+    neighborhood_controller.onBuildDisplay(() => {
+      const parent = pattern_controller_layout?.parent;
+      if (! parent ) return; // Missing parent
+      pattern_controller_layout.resize(parent.width, parent.height);
+    });
+  }
+
+  resize(width:number, height:number) {
+    this.layout.resize(width, height);
+  }
+
+  private formattedLayoutOptions(
+    // grid: CellGrid,
+    neighborhood_controller: NeighborhoodController,
+    persist_at: RuleController,
+    populate_at: RuleController,
+    update_controller: UpdateController
+  ) {
+    return { 
       id: 'root',
       content: {
         user_menu: {
@@ -69,123 +113,78 @@ export class CellUI extends Container {
               }
             },
             middle_user_menu: {
-              content: 'me'
+              content: {
+                rule_block: {
+                  content: {
+                    persist_rule_container: {
+                      content: {
+                        label_container: {
+                          content: {
+                            label: {
+                              content: 'Persist_at: '
+                            }
+                          }
+                        },
+                        input_container: {
+                          content: {
+                            input: {
+                              content: persist_at.getContent()
+                            }
+                          }
+                        }
+                      }
+                    },
+                    populate_rule_container: {
+                      content: {
+                        label_container: {
+                          content: {
+                            label: {
+                              content: 'Populate_at: '
+                            }
+                          }
+                        },
+                        input_container: {
+                          content: {
+                            input: {
+                              content: populate_at.getContent()
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
             },
             lower_user_menu: {
-              content: 'sideways'
+              content: {
+                update_block: {
+                  content: {
+                    direction_controller: {
+                      content: {
+                        step_block: {
+                          content: update_controller.getContent('step_button')
+                        },
+                        auto_block: {
+                          content: update_controller.getContent('auto_button')
+                        }
+                      }
+                    },
+                    timing_block: {
+                      content: {
+                        timing_container: {
+                          content: update_controller.getContent('timer_slider')
+                        }
+                      }
+                    }
+                  }
+                }
+              }
             }
           }
         }
       },
-      globalStyles: {
-        mid_square: {
-          background: 'tomato',
-          position: 'center',
-          maxWidth: 80,
-          maxHeight: 80,
-          overflow: 'hidden',
-        },
-        root: {
-          width: '100%',
-          height: '100%',
-        },
-        user_menu: {
-          background: 'purple',
-          display: 'block',
-          height: '100%',
-          position: 'rightCenter',
-          width: '35%',
-        },
-        upper_user_menu: {
-          background: 'olive',
-          display: 'block',
-          height: '50%',
-          width: '100%',
-        },
-        middle_user_menu: {
-          background: 'teal',
-          width: '100%',
-          height: '25%',
-        },
-        lower_user_menu: {
-          background: 'white',
-          width: '100%',
-          height: '25%',
-        },
-        neighborhood_controller: {
-          background: 'magenta',
-          display: 'block',
-          height: '100%',
-          width: '100%',
-        },
-        pattern_block: {
-          background: 'mistyrose', // debug
-          display: 'block',
-          height: '90%',
-          position: 'leftCenter',
-          width: '80%'
-        },
-        pattern_container: {
-          background: 'cyan',
-          borderRadius: 20,
-          display: 'block',
-          height: '90%',
-          position: 'center',
-          width: '90%'
-        },
-        pattern_controller: {
-          // display: 'block',
-          background: 'green',
-          borderRadius: 20,
-          maxHeight: '90%',
-          maxWidth: '90%',
-          overflow: 'hidden',
-          position: 'center',
-        },
-        range_block: {
-          background: 'midnightblue',
-          display: 'block',
-          height: '90%',
-          // maxHeight: '20%',
-          maxWidth: '20%',
-          position: 'rightCenter',
-          width: '20%'
-        },
-        range_buttons: {
-          display: 'block',
-          background: 'tomato',
-          height: '90%',
-          position: 'leftCenter',
-          width: '80%',
-        },
-        range_up: {
-          background: 'blue',
-          // position: 'top',
-          maxWidth: '95%',
-        },
-        range_down: {
-          background: 'red',
-          position: 'bottom',
-          maxWidth: '95%',
-        }
-      }
-    });
-    // console.log(UIComponentStyles); this.x = this.y = 200
-    this.addChild(this.layout)
-    this.on('added',() => {
-      this.resize(this.parent.width, this.parent.height)
-    });
-    console.log(this.layout);
-    const pattern_controller_layout = this.layout.content.getByID('pattern_controller') as Layout;
-    neighborhood_controller.onBuildDisplay(() => {
-      if (! pattern_controller_layout ) return;
-      const parent = pattern_controller_layout.parent;
-      pattern_controller_layout.resize(parent.width, parent.height);
-    });
-    // neighborhood_controller.setRoot(5);
-  }
-
-  resize(width:number, height:number) {
-    this.layout.resize(width, height);
+      globalStyles: UIComponentStyles
+    }
   }
 }
